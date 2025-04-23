5cfcf939bf3756a04eabd8ef4bae1d96c0a96249
LazyTeenStudios <harleypeterson@gmail.com>
2025-04-22 19:06:27 -0500

    Identified possible crash location

--- HostCrashGuard/HostCrashGuard.cs
+++ HostCrashGuard/HostCrashGuard.cs
@@ -4,6 +4,17 @@ using FrooxEngine;
 using HarmonyLib;
 using ResoniteModLoader;
 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using Elements.Core;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
 namespace HostCrashGuard;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
 public class HostCrashGuard : ResoniteMod {
@@ -17,17 +28,59 @@ public class HostCrashGuard : ResoniteMod {
 		Harmony harmony = new Harmony("com.__Choco__.HostCrashGuard");
 		harmony.Patch(AccessTools.Method(typeof(EngineGatherJob), "RunSessionDownload"), prefix: AccessTools.Method(typeof(PatchMethods), "Before_RunSessionDownload"));
 		harmony.Patch(AccessTools.Method(typeof(EngineGatherJob), "RunSessionDownload"), postfix: AccessTools.Method(typeof(PatchMethods), "After_RunSessionDownload"));
+		harmony.Patch(AccessTools.Method(typeof(LNL_Connection), "OnPeerDisconnected"), prefix: AccessTools.Method(typeof(PatchMethods), "prefix_PeerDisconnected"));
+		harmony.Patch(AccessTools.Method(typeof(LNL_Connection), "OnPeerDisconnected"), postfix: AccessTools.Method(typeof(PatchMethods), "postfix_PeerDisconnected"));
 		harmony.PatchAll();
 	}
 
 	//private async Task RunSessionDownload()
 	class PatchMethods {
+
+		private static bool ohshit = false;
+
 		static void Before_RunSessionDownload(EngineGatherJob __instance) {
 			Msg("Prefix from HostCrashGuard");
 		}
 
 		static void After_RunSessionDownload(EngineGatherJob __instance) {
 			Msg("Postfix from HostCrashGuard");
+		}
+
+		static bool prefix_PeerDisconnected(LNL_Connection __instance, NetPeer peer, DisconnectInfo disconnectInfo) {
+			Msg("Prefix from peer disconnected");
+			Warn(string.Concat(new string[]
+			{
+				"HostCrashGuard: LNL Connection Disconnected: ",
+				(peer != null) ? peer.ToString() : null,
+				", reason: ",
+				disconnectInfo.Reason.ToString(),
+				", socketErrorCode: ",
+				disconnectInfo.SocketErrorCode.ToString()
+			}));
+
+			UniLog.Log(string.Concat(new string[]
+			{
+				"HostCrashGuard: LNL Connection Disconnected: ",
+				(peer != null) ? peer.ToString() : null,
+				", reason: ",
+				disconnectInfo.Reason.ToString(),
+				", socketErrorCode: ",
+				disconnectInfo.SocketErrorCode.ToString()
+			}), true);
+
+			if (disconnectInfo.SocketErrorCode == SocketError.Success) {
+				if (disconnectInfo.Reason == DisconnectReason.Timeout) {
+					Msg("HostCrashGuard: DETECTED CRASH INCOMING!!!!! You are already too late.");
+					ohshit = true;
+					return false; //does not runnign the code to close the socket stop the crash?
+				}
+			}
+			return true;
+			
+		}
+
+		static void postfix_PeerDisconnected(LNL_Connection __instance) {
+			Msg("Postfix from peer disconnected");
 		}
 	}
 }
