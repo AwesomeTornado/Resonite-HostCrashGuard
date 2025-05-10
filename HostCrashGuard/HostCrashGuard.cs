62858b88d7023bf7ee47ccd15c089d60c18b2aef
LazyTeenStudios <harleypeterson@gmail.com>
2025-05-09 23:16:14 -0500

    update version number

--- HostCrashGuard/HostCrashGuard.cs
+++ HostCrashGuard/HostCrashGuard.cs
@@ -14,7 +14,7 @@ using System.Linq;
 namespace HostCrashGuard;
 
 public class HostCrashGuard : ResoniteMod {
-	internal const string VERSION_CONSTANT = "2.2.0"; //Changing the version here updates it in all locations needed
+	internal const string VERSION_CONSTANT = "3.0.0"; //Changing the version here updates it in all locations needed
 	public override string Name => "HostCrashGuard";
 	public override string Author => "__Choco__";
 	public override string Version => VERSION_CONSTANT;
